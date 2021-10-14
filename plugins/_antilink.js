9496a1f7ca53abcf025811c7f6d235840b414790
RevanX4You <92447361+RevanX4You@users.noreply.github.com>
2021-10-14 10:14:40 +0700

    Update _antilink.js

--- plugins/_antilink.js
+++ plugins/_antilink.js
@@ -10,8 +10,8 @@ handler.before = function (m, { user, isAdmin, isBotAdmin }) {
   let link = linkRegex.exec(m.text)
 
   if (chat.antiLink && link) {
-  if (user.isAdmin || user.isSuperAdmin) return m.reply ('Karena Kamu Adalah Admin Group Bot tidak akan Kick Kamu')
- m.reply(`*「 ANTI LINK DETECTOR 」*\n\nTerdeteksi *${name}* telah mengirim link group!\n\nMaaf Kamu akan dikick dari grup ini!`)
+  if (user.isAdmin || user.isSuperAdmin) return m.reply ('sans Kamu Adalah Admin Group Bot tidak akan Kick Kamu')
+ m.reply(`*「 ANTI LINK DETECTOR 」*\n\nTerdeteksi *${name}* telah mengirim link group btw lu ga baca desk dek!\n\nMaaf Kamu akan dikick dari grup ini!`)
    this.groupRemove(m.chat, [m.sender])
   }
 }
