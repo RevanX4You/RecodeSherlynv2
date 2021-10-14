7dbb6f4b4797f2c6fa8312a4fa0f56512b20a3ba
RevanX4You <92447361+RevanX4You@users.noreply.github.com>
2021-10-14 10:12:07 +0700

    Update _anticulik.js

--- plugins/_anticulik.js
+++ plugins/_anticulik.js
@@ -2,8 +2,8 @@ let handler = m => m
 
 handler.all = async function (m) {
     if (m.mtype === 'groupInviteMessage') {
-        m.reply(`untuk mengundang bot ke dalam grup silahkan Ijin Owner terlebih dahulu`)
-        this.sendContact(m.chat, '6281220670449', 'KurrTodあ', m)
+        m.reply(`Lihat Anak Anjg Mau ngulik Gua Makanya sewa Tod:v`)
+        this.sendContact(m.chat, '6285714897009', 'RamadhanXdあ', m)
     }
 }
 
